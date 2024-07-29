5811655a0baac7ed8e3e532e988f225044dc744b
shaymurat <shaymuratoff@ya.ru>
2024-07-30 00:48:38 +0500

    Add files via upload

--- primes.py
+++ primes.py
@@ -3,20 +3,23 @@ numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
 primes = []
 not_primes = []
 
-for i in range(1, len(numbers)):
+for i in numbers:
+    if i == 1:
+        continue
+
     is_prime = True
 
     j = 0
-    while len(primes) > 0 and primes[j] <= numbers[i] // 2:
-        if numbers[i] % primes[j] == 0:
+    while len(primes) > 0 and primes[j] <= i // 2:
+        if i % primes[j] == 0:
             is_prime = False
             break
         j += 1
 
     if is_prime:
-        primes.append(numbers[i])
+        primes.append(i)
     else:
-        not_primes.append(numbers[i])
+        not_primes.append(i)
 
 print('Primes:', primes)
 print('Not Primes:', not_primes)
